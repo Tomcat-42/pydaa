4572550b89933deab872fada7dd6de67956b5d25
Pablo <pablohuggem@gmail.com>
2023-04-03 17:55:06 -0300

    Bump version to 1.4.9

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ with open("LICENSE-3RD-PARTY", "w") as license_file:
 
 setup(
     name="pydaa",
-    version="1.4.8",
+    version="1.4.9",
     author="Pablo Alessandro Santos Hugen",
     author_email="pablohuggem@gmail.com",
     description="python bindings for the daa library",
